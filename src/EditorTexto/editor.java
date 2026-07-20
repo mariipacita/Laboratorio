a52f838a22da218d75a8c86d56f5231e4b0da590
maripacita <mariichavarriajerez29@gmail.com>
2026-07-20 14:30:42 -0600

    Metodos Clase Editor
    Se realiza metodos get, set y constructor de la clase

--- src/EditorTexto/editor.java
+++ src/EditorTexto/editor.java
@@ -9,5 +9,68 @@ package EditorTexto;
  * @author marii
  */
 public class editor {
+    private String nombre;
+    private String ruta;
+    private String contenido;
+    private boolean negrita;
+    private boolean cursiva;
+    private boolean abrir;
+    private boolean guardar;
+    private boolean salir;
+    private boolean copiar;
+    private boolean cortar;
+    private boolean pegar;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public boolean isNegrita() {
+        return negrita;
+    }
+
+    public boolean isGuardar() {
+        return guardar;
+    }
+
+    public boolean isCursiva() {
+        return cursiva;
+    }
+    
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public void setNegrita(boolean negrita) {
+        this.negrita = negrita;
+    }
+
+    public void setCursiva(boolean cursiva) {
+        this.cursiva = cursiva;
+    }
+
+    public void setGuardar(boolean guardar) {
+        this.guardar = guardar;
+    }
+    
+    
+    
     
 }
