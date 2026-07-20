cc39ed3e89eca2a9dc1a932c75a241b3bda636cc
iva <ivallen84@gmail.com>
2026-07-20 14:10:29 -0600

    se crea clase Tareas y PanelAgenda
    se crea la clse tareas, y panelAgenda pero esa será eliminada

---
+++ src/modeloAgenda/tareas.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modeloAgenda;
+
+/**
+ *
+ * @author PC
+ */
+public class tareas {
+    protected String descripcion;
+    protected String estado;
+
+    public tareas(String descripcion, String estado) {
+        this.descripcion = descripcion;
+        this.estado = estado;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+    
+    
+    
+    
+    
+}
