2ec5f1b3d9647d0e958ff56edc58fd6d09ac1c67
maripacita <mariichavarriajerez29@gmail.com>
2026-07-21 00:19:42 -0600

    Vista Editor
    Se cambio por JMenu y JCheckBoxMenuItem las opciones de fuente, negrita y cursiva y se les dio su funcionalidad

--- src/EditorTexto/FrmEditor.java
+++ src/EditorTexto/FrmEditor.java
@@ -36,6 +36,7 @@ public class FrmEditor extends javax.swing.JFrame {
         jFileChooser1 = new javax.swing.JFileChooser();
         jFileChooser2 = new javax.swing.JFileChooser();
         jCheckBox1 = new javax.swing.JCheckBox();
+        jCheckBoxMenuItem2 = new javax.swing.JCheckBoxMenuItem();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
         jMenuBar1 = new javax.swing.JMenuBar();
@@ -47,7 +48,9 @@ public class FrmEditor extends javax.swing.JFrame {
         jMenuItem6 = new javax.swing.JMenuItem();
         jMenuItem5 = new javax.swing.JMenuItem();
         jMenuItem4 = new javax.swing.JMenuItem();
-        jMenuItem8 = new javax.swing.JMenuItem();
+        jMenu3 = new javax.swing.JMenu();
+        jCheckBoxMenuItem3 = new javax.swing.JCheckBoxMenuItem();
+        jCheckBoxMenuItem1 = new javax.swing.JCheckBoxMenuItem();
         jMenuItem9 = new javax.swing.JMenuItem();
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
@@ -64,6 +67,9 @@ public class FrmEditor extends javax.swing.JFrame {
         jMenuItem7.setText("jMenuItem7");
 
         jCheckBox1.setText("jCheckBox1");
+
+        jCheckBoxMenuItem2.setSelected(true);
+        jCheckBoxMenuItem2.setText("jCheckBoxMenuItem2");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -90,20 +96,34 @@ public class FrmEditor extends javax.swing.JFrame {
 
         jMenu2.setText("Editar");
 
+        jMenuItem6.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_C, java.awt.event.InputEvent.CTRL_DOWN_MASK));
         jMenuItem6.setText("Copiar");
         jMenuItem6.addActionListener(this::jMenuItem6ActionPerformed);
         jMenu2.add(jMenuItem6);
 
+        jMenuItem5.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_X, java.awt.event.InputEvent.CTRL_DOWN_MASK));
         jMenuItem5.setText("Cortar");
         jMenuItem5.addActionListener(this::jMenuItem5ActionPerformed);
         jMenu2.add(jMenuItem5);
 
+        jMenuItem4.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_V, java.awt.event.InputEvent.CTRL_DOWN_MASK));
         jMenuItem4.setText("Pegar");
         jMenuItem4.addActionListener(this::jMenuItem4ActionPerformed);
         jMenu2.add(jMenuItem4);
 
-        jMenuItem8.setText("Fuente");
-        jMenu2.add(jMenuItem8);
+        jMenu3.setText("Fuente");
+
+        jCheckBoxMenuItem3.setSelected(true);
+        jCheckBoxMenuItem3.setText("Cursiva");
+        jCheckBoxMenuItem3.addActionListener(this::jCheckBoxMenuItem3ActionPerformed);
+        jMenu3.add(jCheckBoxMenuItem3);
+
+        jCheckBoxMenuItem1.setSelected(true);
+        jCheckBoxMenuItem1.setText("Negrita");
+        jCheckBoxMenuItem1.addActionListener(this::jCheckBoxMenuItem1ActionPerformed);
+        jMenu3.add(jCheckBoxMenuItem1);
+
+        jMenu2.add(jMenu3);
 
         jMenuItem9.setText("Color");
         jMenuItem9.addActionListener(this::jMenuItem9ActionPerformed);
@@ -190,6 +210,30 @@ public class FrmEditor extends javax.swing.JFrame {
         jTextArea1.paste();
     }//GEN-LAST:event_jMenuItem4ActionPerformed
 
+    private void jCheckBoxMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBoxMenuItem1ActionPerformed
+       if (jCheckBoxMenuItem1.isSelected()) {
+
+    jTextArea1.setFont(jTextArea1.getFont().deriveFont(java.awt.Font.BOLD));
+
+} else {
+
+    jTextArea1.setFont(jTextArea1.getFont().deriveFont(java.awt.Font.PLAIN));
+
+}
+    }//GEN-LAST:event_jCheckBoxMenuItem1ActionPerformed
+
+    private void jCheckBoxMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBoxMenuItem3ActionPerformed
+        if (jCheckBoxMenuItem2.isSelected()) {
+
+    jTextArea1.setFont(jTextArea1.getFont().deriveFont(java.awt.Font.ITALIC));
+
+} else {
+
+    jTextArea1.setFont(jTextArea1.getFont().deriveFont(java.awt.Font.PLAIN));
+
+}
+    }//GEN-LAST:event_jCheckBoxMenuItem3ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -217,10 +261,14 @@ public class FrmEditor extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JCheckBox jCheckBox1;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem1;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem2;
+    private javax.swing.JCheckBoxMenuItem jCheckBoxMenuItem3;
     private javax.swing.JFileChooser jFileChooser1;
     private javax.swing.JFileChooser jFileChooser2;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenu jMenu3;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
@@ -229,7 +277,6 @@ public class FrmEditor extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem5;
     private javax.swing.JMenuItem jMenuItem6;
     private javax.swing.JMenuItem jMenuItem7;
-    private javax.swing.JMenuItem jMenuItem8;
     private javax.swing.JMenuItem jMenuItem9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
